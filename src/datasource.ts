825a07c31ae225989be64edab89fffe592f36704
Winstein Martins <sriwinstein@gmail.com>
2023-04-21 23:42:22 -0300

    :sparkles: feat: adding graphql server
    Adding graphql server and all dependencies

---
+++ src/datasource.ts
@@ -0,0 +1,18 @@
+// temporary db
+
+export default [
+  {
+    email: 'tlannister@got.com',
+    firstName: 'Tywin',
+    id: '1',
+    lastName: 'Lannister',
+    password: 'my-very-secured-password',
+  },
+  {
+    email: 'lmormont@got.com',
+    firstName: 'Lyanna',
+    id: '2',
+    lastName: 'Mormont',
+    password: 'my-very-secured-password',
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server';
+import { resolvers } from './resolvers';
+
+type ApolloContext = {};
+
+const GRAPHQL_SCHEMA_PATH = resolve(__dirname, 'schema.graphql');
+
+const typeDefs = readFileSync(GRAPHQL_SCHEMA_PATH, { encoding: 'utf-8' });
+
+const server = new ApolloServer<ApolloContext>({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then((result) => {
+  console.log(`🚀 Server ready at: ${result.url}`);
+});
